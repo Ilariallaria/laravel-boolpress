681eb1a54ef24015e595c2ffb0600787f063da6c
Ilariallaria <hilary.lion@gmail.com>
2022-09-06 00:47:06 +0200

    fix

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
     //    dd('ciao sono la dashborard privata');
         // ora che abbiamo fatto la view, deve tornare la view e non il dd
-        return view ('admin.home');
+
+        $user = Auth::user();
+
+        $data = [
+            'user'=> $user
+        ];
+        return view ('admin.home', $data);
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,5 +2,6 @@
 
 @section('content')
 <h1>Benvenuto nella tua dashboard privata</h1>
-    
+<div>Ciao {{$user->name}}</div>
+<div>La tua mail è: {{$user->email}}</div>
 @endsection
